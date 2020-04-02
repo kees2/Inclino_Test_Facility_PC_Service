0b6b7fb73fa0c394a6738a30549fcf78fdf53025
Kees <kuijl95@gmail.com>
2020-04-02 15:48:11 +0200

    Baro Sensor fix

---
+++ UDP-Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace UDP_Test
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            DataHandler datahandler = new DataHandler();
+
+            while (true)
+            {
+                Thread.Sleep(10000000);
+            }
+        }
+    }   
+}
