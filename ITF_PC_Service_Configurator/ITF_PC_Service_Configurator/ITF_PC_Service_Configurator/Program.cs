089da423f4f74c50a6c302f6df2bb2fd34beb38a
kees <kuijl95@gmail.com>
2020-04-20 13:43:16 +0200

    ITF_PC_Service_Configurator

---
+++ ITF_PC_Service_Configurator/ITF_PC_Service_Configurator/ITF_PC_Service_Configurator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITF_PC_Service_Configurator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte desiredTemperature = 0;
+            while (true)
+            {
+                byte inputValue;
+                Console.Write("Desired temperature: ");
+                string userInput = Console.ReadLine();
+                try
+                {
+                    inputValue = Convert.ToByte(userInput);
+                    if (inputValue > 80)
+                    {
+                        Console.WriteLine("The temperature value of {0} is too high, do you want to damage the system?", inputValue);
+                    }
+                    else
+                    {
+                        desiredTemperature = inputValue;
+                        UDPSend.sendTemp(desiredTemperature);
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Inputvalue is not between 0 and 80");
+                }
+            }
+        }
+    }
+}
---
+++ ITF_PC_Service_Configurator/ITF_PC_Service_Configurator/ITF_PC_Service_Configurator/UDPSend.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ITF_PC_Service_Configurator
+{
+    static class UDPSend
+    {
+        static private Socket sock;
+        static private IPAddress serverAddr;
+        static private IPEndPoint endPoint;
+
+        static private byte[] sendData;
+
+        static public void initUDPSend()
+        {
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverAddr = IPAddress.Parse("192.168.0.10");
+            endPoint = new IPEndPoint(serverAddr, 52256);
+        }
+
+        static public void sendTemp(byte temperature)
+        {
+            byte[] sendData = new byte[1];
+            sendData[0] = temperature;
+            UDPSendData(sendData);
+        }
+
+        static private void UDPSendData(byte[] sendData)
+        {
+            sock.SendTo(sendData, endPoint);
+        }
+
+    }
+}
